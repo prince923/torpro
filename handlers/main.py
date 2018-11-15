5526742b0c419dbdc7608de665f9573e369cdf67
Wyf <2656655648@qq.com>
2018-11-15 10:34:05 +0800

    项目初始化

--- handlers/main.py
+++ handlers/main.py
@@ -5,7 +5,7 @@ class IndexHandler(web.RequestHandler):
     首页，显示用户关注的图片流
     """
     def get(self, *args, **kwargs):
-        self.write('hello,this is Index')
+        self.render('index.html')
 
 
 class ExploreHandler(web.RequestHandler):
@@ -13,7 +13,7 @@ class ExploreHandler(web.RequestHandler):
     发现页,显示最近上传的图片
     """
     def get(self, *args, **kwargs):
-        self.write('hello, This is explore')
+        self.render('explore.html')
 
 
 class PostHandler(web.RequestHandler):
@@ -21,4 +21,4 @@ class PostHandler(web.RequestHandler):
        详情页，显示图片详情
     """
     def get(self, post_id):
-        self.write('hello {}'.format(post_id))
+        self.render('post.html',post_id=post_id)
